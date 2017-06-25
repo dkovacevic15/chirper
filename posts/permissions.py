e61e8977ce978457be5d2cd3f978c27a1b376119
Dusan Kovacevic <dusankovacevic95@gmail.com>
2017-06-25 14:42:17 +0200

    Admins can now edit posts, regardless of authorship

--- posts/permissions.py
+++ posts/permissions.py
@@ -5,6 +5,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
-            return obj.author == request.user
+            return obj.author == request.user || request.user.is_staff
 
 
