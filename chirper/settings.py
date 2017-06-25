9c266bb495d31921b00617808e1a5711c089d441
Dusan Kovacevic <dusankovacevic95@gmail.com>
2017-06-25 19:41:05 +0200

    Added JWT funcionality alongside standard admin interface

--- chirper/settings.py
+++ chirper/settings.py
@@ -130,5 +130,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ),
 }
--- chirper/urls.py
+++ chirper/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/posts/', include('posts.urls')),
     url(r'^api/users/', include('users.urls')),
-    url(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework'))
+    url(r'^api/auth/web/', include('rest_framework.urls',namespace='rest_framework')),
+    url(r'^api/auth/token/', obtain_jwt_token),
 ]
